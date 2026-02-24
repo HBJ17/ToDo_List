c424e63839432f1e037567b1d0cf056e9f05de13
HBJ17 <hbj17672007@gmail.com>
2026-02-24 18:13:48 +0530

    created database

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
 from flask import Flask,render_template
+import sqlite3
 
 app = Flask(__name__)
 
+def get_db():
+    return sqlite3.connect("todo.db")
+
 @app.route("/")
-def home():
-    return render_template("index.html")
+def index():
+    db = get_db()
+    tasks = db.execute("SELECT * FROM tasks").fetchall()
+    db.close()
+    return render_template("index.html",tasks=tasks)
 
 if __name__ == "__main__":
-    app.run(debug="True")
+    db = get_db
+    db.execute("""
+                CREATE TABLE IF NOT EXIST tasks (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                task TEXT,
+                done INTEGER
+            )
+        """)
+    db.close()
+    app.run(debug=True)
 
