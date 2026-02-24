ce95ff0432a5c6fe82d1621c68466807ba6f0aa2
HBJ17 <hbj17672007@gmail.com>
2026-02-24 18:19:44 +0530

    connected form to backend and added task_add routes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request,redirect
 import sqlite3
 
 app = Flask(__name__)
@@ -12,6 +12,15 @@ def index():
     tasks = db.execute("SELECT * FROM tasks").fetchall()
     db.close()
     return render_template("index.html",tasks=tasks)
+
+@app.route("/add",method=["POST"])
+def add():
+    task = request.form["task"]
+    db = get_db()
+    db.execute("INSERT INTO tasks (task,done) VALUES (?,?)",(task,0))
+    db.commit()
+    db.close()
+    return redirect("/")
 
 if __name__ == "__main__":
     db = get_db
